f67081516ad2b23a0b26c2aa32edec0bbe728520
ETS Campinas (CaP/ETS) <ct67ca@bosch.com>
2022-06-23 12:49:14 -0300

    fix dirs

--- section_1/facedetectionv1.py
+++ section_1/facedetectionv1.py
@@ -1,9 +1,9 @@
 from sys import maxsize
 import cv2
 
-image = cv2.imread('pessoas.jpg')
+image = cv2.imread('section_1/pessoas.jpg')
 
-classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Utiliza o xml como classificador
+classifier = cv2.CascadeClassifier('section_1/haarcascade_frontalface_default.xml') #Utiliza o xml como classificador
 grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Converte a imagem para cinza
 
 detection = classifier.detectMultiScale(grayImg, scaleFactor=1.09, minNeighbors=5, minSize=(30,30), maxSize=(40,40)) #fator de escala, vizinhos minimos e tamanho max/min de um obj.
